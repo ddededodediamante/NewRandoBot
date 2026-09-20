561948bf299d3bd7ffdddeaef8f5db91bea84b8f
ddededodediamante <lukitadiamante5@gmail.com>
2026-09-20 17:11:03 -0300

    okay

--- commands/Social/familytree.js
+++ commands/Social/familytree.js
@@ -10,7 +10,7 @@ const {
   collectFamily,
   renderFamilyTree,
   MAX_PEOPLE,
-} = require("../../functions/familyTree.js");
+} = require("../../functions/familytree.js");
 
 const data = new SlashCommandBuilder()
   .setName("familytree")
